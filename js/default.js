a7212a9e6340d9154cb818eb3be06cbac7d4fdad
kayamaguro <g1627017@st.t-kougei.ac.jp>
2017-07-03 17:10:58 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,18 @@
+var separete_time=function(time){
+var sec=time.getSeconds();
+var min=time.getMinutes();
+var hours=time.getDate();
+var month=time.getMonth();
+var year=time.getFullyear();
+return[sec,min,hours,days,month,year];
+}
+
+var now=new Date();
+var counter= separate_time(now);
+document.getElementById('countdown').textContent=
+counter[5]+'年'+
+counter[4]+'月'+
+counter[3]+'日'+
+counter[2]+'時'+
+counter[1]+'分'+
+counter[0]+'秒';
