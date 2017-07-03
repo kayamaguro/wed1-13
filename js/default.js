24ae3edc8fb0f7c9b047c87970dd8442fa012131
kayamaguro <g1627017@st.t-kougei.ac.jp>
2017-07-03 18:10:42 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -20,3 +20,38 @@ counter[3]+'日'+
 counter[2]+'時'+
 counter[1]+'分'+
 counter[0]+'秒';
+
+//クッキー
+function setCookie(c_name,value,expiredays){
+//有効期限
+var extime =new Date().getTime();
+var cltime =new Date(extime +(60*60*24*1000*expiredays));
+var exdate =cltime.touTCString();
+//文字列保存
+var s="";
+s+=c_name+"="+escape(value);
+s+=";path="+location.pathname;
+if(expiredays){
+s+=";expires=";+exdate+";";
+}else{
+ s+=";";
+}
+//クッキー保存
+  document.cookie=s;
+}
+
+//クッキー値
+function getCookie(c_name){
+var st="";
+var ed="";
+if(0 < document.cookie.length){
+//値取り出し
+st=document.cookie.indexOf(c_name+"=";)
+if(st!=-1){
+st=st+c_name.length+1;
+  ed=document.cookie.indexOf(";",st);
+  if(ed==-1)ed=document.cookie.substring(st,ed));
+}
+}
+  return"";
+}
